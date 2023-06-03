5e6a977a2dda1da6c7640073d4bdf3d6bfc908d9
Hacker-Wolf <alex.krysiuk@icloud.com>
2023-06-03 15:10:52 +0200

    GetDogById method implementation

--- src/Codecool.MissingDog/Repository/DogRepository.cs
+++ src/Codecool.MissingDog/Repository/DogRepository.cs
@@ -27,7 +27,7 @@ namespace Codecool.MissingDog.Repository
         /// <returns> IEnumerable of all Dogs instances and nulls. </returns>
         public IEnumerable<Dog> GetAllDogs()
         {
-            throw new NotImplementedException();
+            return _data.Dogs;
         }
 
         /// <summary>
@@ -37,7 +37,7 @@ namespace Codecool.MissingDog.Repository
         /// <returns> Dog instance or null. </returns>
         public Dog GetDogById(int id)
         {
-            throw new NotImplementedException();
+            return _data.Dogs.FirstOrDefault(dog => dog?.Id == id);
         }
 
         /// <summary>
